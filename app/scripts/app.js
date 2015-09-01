a7e8e3dcce98f4952f4ad7470b24f0ecf9371ead
Scthe <marcin1113C@gmail.com>
2015-09-01 20:08:13 +0200

    Add article route

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,12 +12,17 @@ angular
   .module('redditAngularApp', [
     'ngRoute'
   ])
-  .config(function ($routeProvider) {
+  .config(function($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/article-list.html',
         controller: 'ArticleListCtrl',
         controllerAs: 'article-list'
+      })
+      .when('/article/:articleId', {
+        templateUrl: 'views/article.html',
+        controller: 'ArticleCtrl',
+        controllerAs: 'article'
       })
       .otherwise({
         redirectTo: '/'
---
+++ app/scripts/controllers/article.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name redditAngularApp.controller:ArticleCtrl
+ * @description
+ * # ArticleCtrl
+ * Controller of the redditAngularApp
+ */
+angular.module('redditAngularApp')
+  .controller('ArticleCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
