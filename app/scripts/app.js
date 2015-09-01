4c07bf7f739723aefec804df59a2b48c96f2f9ae
Scthe <marcin1113C@gmail.com>
2015-09-01 20:05:06 +0200

    Remove about and main routes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,16 +15,6 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .when('/articleList', {
         templateUrl: 'views/article-list.html',
         controller: 'ArticleListCtrl',
         controllerAs: 'article-list'
