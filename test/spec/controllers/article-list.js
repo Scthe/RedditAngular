d4dc63f3bf04c2dd8d6cfc8cd7bc1f82695ae47c
Scthe <marcin1113C@gmail.com>
2015-09-03 18:48:01 +0200

    Refactor article-list controller test to use promises

--- test/spec/controllers/article-list.js
+++ test/spec/controllers/article-list.js
@@ -5,64 +5,109 @@ describe('Controller: ArticlelistCtrl', function() {
   // load the controller's module
   beforeEach(module('redditAngularApp'));
 
+  var scope, $q, $rootScope, $controller, routeParams, articleRepositoryService;
 
-  var scope, $controller;
+  beforeEach(function() {
+    module(function($provide) {
+      articleRepositoryService = {};
+      $provide.service('ArticlesRepositoryService', function() {
+        return articleRepositoryService;
+      });
+    });
+  });
 
-  beforeEach(inject(function(_$controller_) {
+  beforeEach(inject(function(_$rootScope_, _$q_, _$controller_) {
     scope = {};
+    routeParams = {};
+    $q = _$q_;
+    $rootScope = _$rootScope_;
     $controller = _$controller_;
   }));
 
-  it('sets the error message if articles are unavailable', function() {
-    instantiateController({
-      getListSuccess: false
+  function instantiateController() {
+    return $controller('ArticleListCtrl', {
+      $scope: scope,
+      $routeParams: routeParams,
+      ArticlesRepositoryService: articleRepositoryService
     });
-    // Propagate promise resolution to 'then' functions using $apply().
-    // $rootScope.$apply();
+  }
+
+  it('sets the error message if articles are unavailable', function() {
+    articleRepositoryService.refreshList = function() {
+      return $q.reject('some error');
+    };
+
+    instantiateController();
+    $rootScope.$apply();
+
     expect(scope.errorMessage).toBeDefined();
   });
 
-
-
   it('has articles list', function() {
-    instantiateController({
-      getListSuccess: true
-    });
-    // Propagate promise resolution to 'then' functions using $apply().
-    // $rootScope.$apply();
+    var articles = ['article1', 'article2']
+    articleRepositoryService.refreshList = function() {
+      return $q.when(articles);
+    };
+
+    instantiateController();
+    $rootScope.$apply();
+
     expect(scope.list).toBeDefined();
   });
 
+
   describe('pagination', function() {
+
     describe('-previous page-', function() {
       it('gives correct link if previous page exists', function() {
-        instantiateController({
-          getListSuccess: true,
-          articleBefore: '0000' // TODO use faker
-        });
+        articleRepositoryService.refreshList = function() {
+          return $q.when({});
+        };
+        articleRepositoryService.articleBefore = '0000';
+
+        instantiateController();
+        $rootScope.$apply();
+
         expect(scope.linkPrevPage()).toBe('before/0000');
       });
 
       it('returns empty link if there is no previous page', function() {
-        instantiateController({});
+        articleRepositoryService.refreshList = function() {
+          return $q.when({});
+        };
+
+        instantiateController();
+        $rootScope.$apply();
+
         expect(scope.linkPrevPage()).toBe('');
       });
     });
 
     describe('-next page-', function() {
       it('gives correct link if next page exists', function() {
-        instantiateController({
-          getListSuccess: true,
-          articleAfter: '0000' // TODO use faker
-        });
+        articleRepositoryService.refreshList = function() {
+          return $q.when({});
+        };
+        articleRepositoryService.articleAfter = '0000';
+
+        instantiateController();
+        $rootScope.$apply();
+
         expect(scope.linkNextPage()).toBe('after/0000');
       });
 
       it('returns empty link if there is no next page', function() {
-        instantiateController({});
+        articleRepositoryService.refreshList = function() {
+          return $q.when({});
+        };
+
+        instantiateController();
+        $rootScope.$apply();
+
         expect(scope.linkNextPage()).toBe('');
       });
     });
+
   });
 
   describe('article link', function() {
@@ -73,62 +118,52 @@ describe('Controller: ArticlelistCtrl', function() {
         },
         expectUrl = '/article/' + fakeArticle.subreddit + '/' + fakeArticle.id;
 
-      instantiateController({});
+      articleRepositoryService.refreshList = function() {
+        return $q.when({});
+      };
+      instantiateController();
+      $rootScope.$apply();
+
       expect(scope.linkForArticle(fakeArticle)).toBe(expectUrl);
     });
 
     it('is empty if there is no article', function() {
-      instantiateController({});
+      articleRepositoryService.refreshList = function() {
+        return $q.when({});
+      };
+      instantiateController();
+      $rootScope.$apply();
+
       expect(scope.linkForArticle()).toBe('');
     });
   });
 
   describe('route parameters', function() {
     it('are used to read articles list', function() {
-      instantiateController({}, '0000', '1111');
-      expect(scope.__service.refreshList).toHaveBeenCalledWith('0000', '1111');
+      articleRepositoryService.refreshList = function() {
+        return $q.when({});
+      };
+      spyOn(articleRepositoryService, 'refreshList').and.callThrough();
+      routeParams.direction = '0000';
+      routeParams.last = '1111';
+
+      instantiateController();
+      $rootScope.$apply();
+
+      expect(articleRepositoryService.refreshList).toHaveBeenCalledWith('0000', '1111');
     });
 
     it('can be empty', function() {
-      instantiateController({});
-      expect(scope.__service.refreshList).toHaveBeenCalledWith(undefined, undefined);
+      articleRepositoryService.refreshList = function() {
+        return $q.when({});
+      };
+      spyOn(articleRepositoryService, 'refreshList').and.callThrough();
+
+      instantiateController();
+      $rootScope.$apply();
+
+      expect(articleRepositoryService.refreshList).toHaveBeenCalledWith(undefined, undefined);
     });
   });
-
-  function instantiateController(articlesStatus, routeDir, routeLast) {
-    // TODO major refactor
-    var articles = ['article1', 'article2'],
-      mockPromise = {
-        // each, this extremely stupid way to deal with testing of async
-        // functions, but I hate testing async code. And documentation for
-        // $rootScope.$apply(); is vague at best.
-        then: function(succCallback, errCallback) {
-          if (articlesStatus.getListSuccess) {
-            succCallback(articles);
-          } else {
-            errCallback('something went wrong', 404);
-          }
-        }
-      },
-      mockService = {
-        refreshList: function() {
-          return mockPromise;
-        },
-        articleBefore: articlesStatus.articleBefore,
-        articleAfter: articlesStatus.articleAfter
-      };
-    spyOn(mockService, 'refreshList').and.callThrough();
-
-    scope.__service = mockService;
-
-    return $controller('ArticleListCtrl', {
-      $scope: scope,
-      $routeParams: {
-        direction: routeDir,
-        last: routeLast
-      },
-      ArticlesRepositoryService: mockService
-    });
-  }
 
 });
