e42f4420dd23a95a32e48b99692fa1de88c31605
Scthe <marcin1113C@gmail.com>
2015-09-03 18:31:20 +0200

    Refactor article controller test to use promises

--- test/spec/controllers/article.js
+++ test/spec/controllers/article.js
@@ -5,29 +5,57 @@ describe('Controller: ArticleCtrl', function() {
   // load the controller's module
   beforeEach(module('redditAngularApp'));
 
-  var scope, $controller;
+  var scope, $q, $rootScope, $controller, routeParams, articleRepositoryService;
 
-  beforeEach(inject(function(_$controller_) {
+  beforeEach(function() {
+    module(function($provide) {
+      articleRepositoryService = {};
+      $provide.service('ArticlesRepositoryService', function() {
+        return articleRepositoryService;
+      });
+    });
+  });
+
+  beforeEach(inject(function(_$rootScope_, _$q_, _$controller_) {
     scope = {};
+    routeParams = {};
+    $q = _$q_;
+    $rootScope = _$rootScope_;
     $controller = _$controller_;
   }));
 
+  function instantiateController() {
+    return $controller('ArticleCtrl', {
+      $scope: scope,
+      $routeParams: routeParams,
+      ArticlesRepositoryService: articleRepositoryService
+    });
+  }
+
   it('sets the error message if article is unavailable', function() {
+    articleRepositoryService.get = function() {
+      return $q.reject('some error');
+    };
+
     instantiateController();
-    // Propagate promise resolution to 'then' functions using $apply().
-    // $rootScope.$apply();
+    $rootScope.$apply();
+
     expect(scope.errorMessage).toBeDefined();
     expect(scope.article).toBeUndefined();
-
   });
+
 
   it('has article', function() {
     var fakeArticle = {
       title: '000'
     };
-    instantiateController(fakeArticle);
-    // Propagate promise resolution to 'then' functions using $apply().
-    // $rootScope.$apply();
+    articleRepositoryService.get = function() {
+      return $q.when(fakeArticle);
+    };
+
+    instantiateController();
+    $rootScope.$apply();
+
     expect(scope.article).toBeDefined(fakeArticle);
     expect(scope.errorMessage).toBeUndefined();
   });
@@ -37,53 +65,41 @@ describe('Controller: ArticleCtrl', function() {
       var fakeArticle = {
         name: '000'
       };
-      instantiateController(fakeArticle);
+      articleRepositoryService.get = function() {
+        return $q.when(fakeArticle);
+      };
+
+      instantiateController();
+      $rootScope.$apply();
+
       expect(scope.getArticleId()).toBe(fakeArticle.name);
     });
 
     it('is empty if there is no article', function() {
+      articleRepositoryService.get = function() {
+        return $q.reject('some error');
+      };
+
       instantiateController();
+      $rootScope.$apply();
+
       expect(scope.getArticleId()).toBe('');
     });
 
   });
 
   it('uses route parameters to find article', function() {
-    instantiateController({}, '0000', '1111');
-    expect(scope.__service.get).toHaveBeenCalledWith('0000', '1111');
+    articleRepositoryService.get = function() {
+      return $q.when({});
+    };
+    spyOn(articleRepositoryService, 'get').and.callThrough();
+    routeParams.articleId = '0000';
+    routeParams.subreddit = '1111';
+
+    instantiateController();
+    $rootScope.$apply();
+
+    expect(articleRepositoryService.get).toHaveBeenCalledWith('0000', '1111');
   });
-
-  function instantiateController(article, routeId, routeSub) {
-    // TODO major refactor
-    var mockPromise = {
-        // each, this extremely stupid way to deal with testing of async
-        // functions, but I hate testing async code. And documentation for
-        // $rootScope.$apply(); is vague at best.
-        then: function(succCallback, errCallback) {
-          if (article !== undefined) {
-            succCallback(article);
-          } else {
-            errCallback('something went wrong', 404);
-          }
-        }
-      },
-      mockService = {
-        get: function() {
-          return mockPromise;
-        }
-      };
-    spyOn(mockService, 'get').and.callThrough();
-
-    scope.__service = mockService;
-
-    return $controller('ArticleCtrl', {
-      $scope: scope,
-      $routeParams: {
-        articleId: routeId,
-        subreddit: routeSub
-      },
-      ArticlesRepositoryService: mockService
-    });
-  }
 
 });
